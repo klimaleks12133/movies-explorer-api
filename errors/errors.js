158b9010efd18ed369b236af8fe9b4275ec92194
klimaleks12133 <120417478+klimaleks12133@users.noreply.github.com>
2024-02-15 21:38:43 +0300

    Feat: add functionality

---
+++ errors/errors.js
@@ -0,0 +1,13 @@
+const BadRequestError = require('./BadRequestError');
+const UnauthorizedError = require('./UnauthorizedError');
+const ForbiddenError = require('./ForbiddenError');
+const ConflictError = require('./ConflictError');
+const NotFoundError = require('./NotFoundError');
+
+module.exports = {
+  BadRequestError,
+  UnauthorizedError,
+  ForbiddenError,
+  ConflictError,
+  NotFoundError,
+};
